9944be7f45ad1a88917652d4da1024d8fe97aa00
ratulahmed <ratulahmed1@gmail.com>
2021-08-27 20:36:43 -0400

    added cors headers

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.use('', (req, res, next) => {
+/**Middleware */
+app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader("Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    next();
+})
+
+/* Routes  */
+app.post('/api/auth/signup', (req, res, next) => {
+    console.log(req.body);
+    
     const myRes = 
     {
-        message: "Hello from the server"
+        message: "User has been created"
     }
     res.status(200).json(myRes);
 })
