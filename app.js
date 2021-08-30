ee834bf7320e852681db2dca3da53e7d903a0700
ratulahmed <ratulahmed1@gmail.com>
2021-08-29 22:18:27 -0400

    Separated auth routes from app.js and cleaned up imports

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 
 const app = express();
-const User = require('./models/user');
 const db = require('./config/database');
-const bcrypt = require('bcrypt');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const checkAuth = require('./middleware/check-auth');
+const authRoutes = require('./routes/auth');
 
 db
   .authenticate()
@@ -22,56 +19,57 @@ app.use(express.json())
 app.use(cors());
 app.options('*', cors());
 
+app.use('/api/auth', authRoutes)
 
-/* Routes  */
-app.post('/api/auth/signup', async(req, res, next) => {
-    req.body.password = await bcrypt.hash(req.body.password, 10);
-    const {email, username, password } = req.body;
-    try {
-        const user = await User.create({email, username, password})
-        return res.json(user);
-    } catch(err){
-        console.log(err);
-        return res.status(500);
-    }
-});
+// /* Routes  */
+// app.post('/api/auth/signup', async(req, res, next) => {
+//     req.body.password = await bcrypt.hash(req.body.password, 10);
+//     const {email, username, password } = req.body;
+//     try {
+//         const user = await User.create({email, username, password})
+//         return res.json(user);
+//     } catch(err){
+//         console.log(err);
+//         return res.status(500);
+//     }
+// });
 
-app.post('/api/auth/login', async(req, res, next) => {
-    console.log(req.headers.authorization)
-    user = await User.findOne({
-        where: {
-            email: req.body.email
-        }
-    })    
-    try {
-        result = await bcrypt.compare(req.body.password, user.password);
-    } catch(err){
-        console.log(err);
-    }
+// app.post('/api/auth/login', async(req, res, next) => {
+//     console.log(req.headers.authorization)
+//     user = await User.findOne({
+//         where: {
+//             email: req.body.email
+//         }
+//     })    
+//     try {
+//         result = await bcrypt.compare(req.body.password, user.password);
+//     } catch(err){
+//         console.log(err);
+//     }
 
-    if(result === true) {
-        const token = jwt.sign(
-            { email: user.email, userId: user.id}, 
-            'our_json_secret', 
-            { expiresIn: '1h'});
+//     if(result === true) {
+//         const token = jwt.sign(
+//             { email: user.email, userId: user.id}, 
+//             'our_json_secret', 
+//             { expiresIn: '1h'});
 
         
-        return res.status(200).json({
-            user: user,
-            token: token
-        });
-    }
-    else {
-        return res.status(404).json({
-            message: "Incorrect password/email combination"
-        })
-    }
-});
+//         return res.status(200).json({
+//             user: user,
+//             token: token
+//         });
+//     }
+//     else {
+//         return res.status(404).json({
+//             message: "Incorrect password/email combination"
+//         })
+//     }
+// });
 
-app.get('/api/auth/test',
-checkAuth,
-(req, res, next) =>{
-    res.status(200).json({message: 'We verified the token is correct'});
-})
+// app.get('/api/auth/test',
+// checkAuth,
+// (req, res, next) =>{
+//     res.status(200).json({message: 'We verified the token is correct'});
+// })
 
 module.exports = app;
