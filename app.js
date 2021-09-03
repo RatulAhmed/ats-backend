03b06490c5bc14f9f02cb8ace6fb6d29d51b6319
ratulahmed <ratulahmed1@gmail.com>
2021-09-02 23:35:45 -0400

    Made api call to external data - need to parse the json response WIP

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const db = require('./config/database');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+const oddRoutes = require('./routes/odd');
 
+/* Database Connection */
 db
   .authenticate()
   .then(() => {
@@ -14,62 +16,12 @@ db
     'Unable to connect to the database:', err
   });  
 
-/**Middleware */
+/* Middleware */
 app.use(express.json())
 app.use(cors());
 app.options('*', cors());
 
-app.use('/api/auth', authRoutes)
-
-// /* Routes  */
-// app.post('/api/auth/signup', async(req, res, next) => {
-//     req.body.password = await bcrypt.hash(req.body.password, 10);
-//     const {email, username, password } = req.body;
-//     try {
-//         const user = await User.create({email, username, password})
-//         return res.json(user);
-//     } catch(err){
-//         console.log(err);
-//         return res.status(500);
-//     }
-// });
-
-// app.post('/api/auth/login', async(req, res, next) => {
-//     console.log(req.headers.authorization)
-//     user = await User.findOne({
-//         where: {
-//             email: req.body.email
-//         }
-//     })    
-//     try {
-//         result = await bcrypt.compare(req.body.password, user.password);
-//     } catch(err){
-//         console.log(err);
-//     }
-
-//     if(result === true) {
-//         const token = jwt.sign(
-//             { email: user.email, userId: user.id}, 
-//             'our_json_secret', 
-//             { expiresIn: '1h'});
-
-        
-//         return res.status(200).json({
-//             user: user,
-//             token: token
-//         });
-//     }
-//     else {
-//         return res.status(404).json({
-//             message: "Incorrect password/email combination"
-//         })
-//     }
-// });
-
-// app.get('/api/auth/test',
-// checkAuth,
-// (req, res, next) =>{
-//     res.status(200).json({message: 'We verified the token is correct'});
-// })
-
+/* Routes */
+app.use('/api/auth', authRoutes);
+app.use('/api/odd', oddRoutes);
 module.exports = app;
