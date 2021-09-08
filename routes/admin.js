637a1dc1d58a350ebdc5ce9ee52844fcf74c68c5
ratulahmed <ratulahmed1@gmail.com>
2021-09-08 17:00:11 -0400

    Moved getting odds into admin route as it is an admin function

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,12 @@ const https = require('https');
 const router = express.Router();
 
 const Odd = require('../models/odd');
-const { Op } = require('sequelize');
 
 const apiKey = '4123da3ce51d33a0a5733928bda4fdbe';
 const url = 'https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?regions=us&oddsFormat=american&markets=spreads&apiKey='
 
 
-router.get('', async(req, res, next) => {
+router.get('updateOdd', async(req, res, next) => {
     let data = "";
     https.get(url + '' + apiKey, res =>{
         
@@ -20,27 +19,20 @@ router.get('', async(req, res, next) => {
         res.on("end", () => {
             let json = JSON.parse(data); 
             let homeTeam, awayTeam, homeSpread, awaySpread;
-            // let dkOdds = json[0].bookmakers[2].markets[0].outcomes; 
+            
+            /* TODO - currently hardcoded as 16 - get number of games for the curr week */
             for(i = 0; i < 16; i++) {
-                // console.log(json[i].bookmakers[2].markets[0].outcomes);
-                // console.log('the Home team is ', json[i].home_team);
-                // console.log('The away team is ', json[i].away_team)
-                // console.log(json[i].bookmakers[2].markets[0]);
                 homeTeam = json[i].home_team;
                 awayTeam = json[i].away_team;
-
-                console.log(homeTeam + ' ' + awayTeam);
 
                 if(json[i].home_team === homeTeam && json[i].away_team === awayTeam) {
                     if(homeTeam === json[i].bookmakers[2].markets[0].outcomes[0].name) {
                         homeSpread = json[i].bookmakers[2].markets[0].outcomes[0].point;
                         awaySpread = json[i].bookmakers[2].markets[0].outcomes[1].point
-                        console.log('We are setting the spread in first if: Homespread' + homeSpread + ' away spread '+ awaySpread);
                     }
                     else if(homeTeam === json[i].bookmakers[2].markets[0].outcomes[1].name) {
                         awaySpread = json[i].bookmakers[2].markets[0].outcomes[0].point
                         homeSpread = json[i].bookmakers[2].markets[0].outcomes[1].point;
-                        console.log('We are setting the spread in else if: Homespread' + homeSpread + ' away spread '+ awaySpread);
                     }
                 }
                 Odd.update({
@@ -49,7 +41,7 @@ router.get('', async(req, res, next) => {
                 }, { where: {
                     home_team: homeTeam,
                     away_team: awayTeam,
-                    week: '1' //TODO this needs to come from request
+                    week: '1' //TODO this needs to come from config tables
                 }})
             }
         })
@@ -58,8 +50,14 @@ router.get('', async(req, res, next) => {
         })
 
     })
-    return res.json({message: 'sucess'});
+    return res.json({ message: 'Successfully updated the odds'});
 })
+
+
+// we need a a route to trigger - update scores
+
+
+// we need a route to trigger - lock all odds for current week - lock odd for thursday?
 
 
 module.exports = router;
