ec60979d6ced6929b57bd8affc78cb7754acd86d
ratulahmed <ratulahmed1@gmail.com>
2021-09-11 01:22:49 -0400

    Added a user_choice model and am a test route to retreieve selections - WIP need to do it by id from frontend

---
+++ models/user_choice.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database');
+const User = require('../models/user');
+const Odd = require('./odd');
+
+const User_Choice = db.define('user_choice', {
+
+    odd_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+            model: Odd,
+            key: 'id'
+        }
+
+    },
+    user_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    },
+    selection: {
+        type: Sequelize.DataTypes.STRING
+    }
+});
+
+module.exports= User_Choice;
--- routes/odd.js
+++ routes/odd.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const Odd = require('../models/odd');
 const Nfl_config = require('../models/nfl_config');
 
+const User_Choice = require('../models/user_choice');
+
 // TODO this routes should pass in a userid as a path param
 router.get('/', async (req, res, next) => {
     try {
@@ -36,6 +38,23 @@ router.get('/', async (req, res, next) => {
         message: 'Error'
     })
 }
+});
+
+router.get('/test', async(req, res, next) => {
+    
+    try {
+        const choices = await User_Choice.findAll({
+            where: {
+                user_id: 18
+            }
+        });
+        return res.json(choices);
+    } catch(err) {
+
+    }
+    
+    
+    return res.json('test');
 
 });
 
