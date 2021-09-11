42928224c97fe318f8e335b2ce615534290b3f37
ratulahmed <ratulahmed1@gmail.com>
2021-09-10 22:10:18 -0400

    added more info to login response and also odds are sent back in asc order by id

--- routes/odd.js
+++ routes/odd.js
@@ -4,11 +4,12 @@ const router = express.Router();
 const Odd = require('../models/odd');
 const Nfl_config = require('../models/nfl_config');
 
+// TODO this routes should pass in a userid as a path param
 router.get('/', async (req, res, next) => {
     try {
         nfl_config = await Nfl_config.findAll({
             attributes: ['currentWeek'],
-            raw: true
+
         })
     } catch(err) {
         console.log(err);
@@ -19,8 +20,16 @@ router.get('/', async (req, res, next) => {
     const weeklyOdds = await Odd.findAll({
         where: {
             week: currentWeek
-        }
+        },
+        order: [
+            ['id', 'ASC']
+        ]
     });
+
+    for(let i = 0 ; i < weeklyOdds.length; i++) {
+        console.log(weeklyOdds[i]);
+    }
+
     return res.json(weeklyOdds);
 } catch(err) {
     return res.status(404).json({
