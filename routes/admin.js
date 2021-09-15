145b22c7d2d038a75c189fb1d2ed0598ae9f3863
ratulahmed <ratulahmed1@gmail.com>
2021-09-15 17:22:41 -0400

    forgot / in admin updateOdd route

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,7 @@ const apiKey = '4123da3ce51d33a0a5733928bda4fdbe';
 const url = 'https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?regions=us&oddsFormat=american&markets=spreads&apiKey='
 
 
-router.get('updateOdd', async(req, res, next) => {
+router.get('/updateOdd', async(req, res, next) => {
     let data = "";
     https.get(url + '' + apiKey, res =>{
         
